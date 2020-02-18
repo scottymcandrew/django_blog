d966a53df6bd41f3f3f516d467a472edd85383a2
Scott McAndrew <scottymcandrew@outlook.com>
2020-02-18 21:45:18 +0000

    Added media. Corrected syntax issue in edit_post url, corrected syntax issue for datetime order_by in views

--- posts/urls.py
+++ posts/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     url(r'^$', get_posts, name='get_posts'),
     url(r'^(?P<pk>\d+)/$', post_detail, name='post_detail'),
     url(r'^new/$', create_or_edit_post, name='new_post'),
-    url(r'^(?P<pk>\d+)/edit/$', create_or_edit_post, name='edit+post')
+    url(r'^(?P<pk>\d+)/edit/$', create_or_edit_post, name='edit_post')
 ]
